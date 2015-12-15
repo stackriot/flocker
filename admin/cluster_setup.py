b1beb57775208cf818b127cd47b32413ffa84c17
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-12-15 11:37:56 +0200

    use FlockerClient API instead of configuration/_compose interface

--- admin/cluster_setup.py
+++ admin/cluster_setup.py
@@ -5,19 +5,14 @@ Set up a flocker cluster.
 
 import sys
 import yaml
-from copy import deepcopy
 from itertools import repeat
-from json import dumps
 from pipes import quote as shell_quote
 
-from eliot import add_destination, write_failure, FileDestination
-
-from treq import json_content
+from eliot import add_destination, FileDestination
 
 from twisted.internet.defer import inlineCallbacks
 from twisted.python.filepath import FilePath
 from twisted.python.usage import UsageError
-from twisted.web.http import OK
 
 from .acceptance import (
     CommonOptions,
@@ -27,8 +22,7 @@ from .acceptance import (
     get_trial_environment,
 )
 
-from flocker.acceptance.testtools import check_and_decode_json
-from flocker.ca import treq_with_authentication
+from flocker.apiclient import FlockerClient
 from flocker.common import gather_deferreds, loop_until
 from flocker.control.httpapi import REST_API_PORT
 
@@ -37,10 +31,11 @@ class RunOptions(CommonOptions):
     description = "Set up a flocker cluster."
 
     optParameters = [
+        ['image', None, None, 'Image to use for application containers'],
         ['apps-per-node', None, 0, 'Number of application containers per node',
          int],
-        ['app-template', None, None,
-         'Configuration to use for each application container'],
+        ['vols-per-node', None, 0, 'Number of data volumes per node',
+         int],
         ['purpose', None, 'testing',
          "Purpose of the cluster recorded in its metadata where possible"],
     ]
@@ -66,9 +61,9 @@ class RunOptions(CommonOptions):
         if self['app-template'] is not None:
             template_file = FilePath(self['app-template'])
             self['template'] = yaml.safe_load(template_file.getContent())
-        elif self['apps-per-node'] > 0:
+        if self['apps-per-node'] > 0 and self['image'] is None:
             raise UsageError(
-                "app-template parameter must be provided if apps-per-node > 0"
+                "image parameter must be provided if apps-per-node > 0"
             )
 
 
@@ -123,12 +118,28 @@ def main(reactor, args, base_path, top_level):
                                )
         gather_deferreds(results)
 
-        if options['apps-per-node'] > 0:
-            config = _build_config(cluster, options['template'],
-                                   options['apps-per-node'])
-            result = yield _configure(reactor, cluster, config)
-        else:
-            result = 0
+        client = _get_flocker_client(reactor, cluster)
+        nodes = yield _get_nodes(reactor, client, cluster)
+
+        results = []
+        for node in nodes:
+            for i in range(options['apps-per-node']):
+                name = u"app_%s_%d" % (node.public_address, i)
+                name = name.replace(u".", u"_")
+                results.append(
+                    client.create_container(node.uuid, name, options['image'])
+                )
+        yield gather_deferreds(results)
+
+        print("Creaing volumes...")
+        print("Please note that they are not automatically destroyed")
+        results = []
+        for node in nodes:
+            for i in range(options['vols-per-node']):
+                results.append(client.create_dataset(node.uuid))
+        yield gather_deferreds(results)
+
+        result = 0
 
     except Exception:
         result = 1
@@ -153,106 +164,23 @@ def main(reactor, args, base_path, top_level):
     raise SystemExit(result)
 
 
-def _build_config(cluster, application_template, per_node):
-    """
-    Build a flocker deployment configuration for the given cluster
-    and parameters.
-    The configuration consists of identically configured applications
-    (containers) uniformly spread over all cluster nodes.
-
-    :param flocker.provision._common.Cluster cluster: The target cluster.
-    :param dict application_template: A dictionary that provides configuration
-                                      for an individual application.
-    :param int per_node: The number of applications to deploy on each cluster
-                         node.
-    :return dict: The deployment configuration.
-    """
-    application_root = {}
-    applications = {}
-    application_root["version"] = 1
-    application_root["applications"] = applications
-    for node in cluster.agent_nodes:
-        for i in range(per_node):
-            name = "app_%s_%d" % (node.private_address, i)
-            applications[name] = deepcopy(application_template)
-
-    deployment_root = {}
-    nodes = {}
-    deployment_root["nodes"] = nodes
-    deployment_root["version"] = 1
-    for node in cluster.agent_nodes:
-        nodes[node.private_address] = []
-        for i in range(per_node):
-            name = "app_%s_%d" % (node.private_address, i)
-            nodes[node.private_address].append(name)
-
-    return {"applications": application_root,
-            "deployment": deployment_root}
-
-
-def _configure(reactor, cluster, configuration):
-    """
-    Configure the cluster with the given deployment configuration.
-
-    :param reactor: The reactor to use.
-    :param flocker.provision._common.Cluster cluster: The target cluster.
-    :param dict configuration: The deployment configuration.
-    :return Deferred: Deferred that fires when the configuration is pushed
-                      to the cluster's control agent.
-    """
-    base_url = b"https://{}:{}/v1".format(
-        cluster.control_node.address, REST_API_PORT
-    )
+def _get_flocker_client(reactor, cluster):
+    control_node = cluster.control_node.address
     certificates_path = cluster.certificates_path
     cluster_cert = certificates_path.child(b"cluster.crt")
     user_cert = certificates_path.child(b"user.crt")
     user_key = certificates_path.child(b"user.key")
-    body = dumps(configuration)
-    treq_client = treq_with_authentication(
-        reactor, cluster_cert, user_cert, user_key)
+    return FlockerClient(reactor, control_node, REST_API_PORT,
+                         cluster_cert, user_cert, user_key)
 
+
+def _get_nodes(reactor, client, cluster):
     def got_all_nodes():
-        d = treq_client.get(
-            base_url + b"/state/nodes",
-            persistent=False
+        d = client.list_nodes()
+        d.addCallbacks(
+            lambda nodes: len(nodes) == len(cluster.agent_nodes),
+            lambda _: False
         )
-        d.addCallback(check_and_decode_json, OK)
-        d.addCallback(
-            lambda nodes: len(nodes) >= len(cluster.agent_nodes)
-        )
-        d.addErrback(write_failure, logger=None)
         return d
-
     got_nodes = loop_until(reactor, got_all_nodes, repeat(1, 300))
-
-    def do_configure(success):
-        if not success:
-            return 1
-
-        posted = treq_client.post(
-            base_url + b"/configuration/_compose", data=body,
-            headers={b"content-type": b"application/json"},
-            persistent=False
-        )
-
-        def got_response(response):
-            if response.code != OK:
-                sys.stderr.write("Got response %d\n" % (response.code,))
-                d = json_content(response)
-
-                def got_error(error):
-                    if isinstance(error, dict):
-                        error = error[u"description"] + u"\n"
-                    else:
-                        error = u"Unknown error: " + unicode(error) + "\n"
-                    sys.stderr.write(error)
-                    return 1
-                d.addCallbacks(got_error, lambda _: 1)
-                return d
-            else:
-                return 0
-        posted.addCallback(got_response)
-        return posted
-
-    configured = got_nodes.addCallback(do_configure)
-    return configured
+    return got_nodes.addCallback(lambda _: client.list_nodes())
